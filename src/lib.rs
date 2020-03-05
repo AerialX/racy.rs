c6a92ecd4a96e520167c556a0cde99ea7b59305a
AerialX <aerialx@users.noreply.github.com>
2020-03-05 14:30:19 -0800

    RecklessCell and organization

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-#![no_std]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(feature = "unstable", feature(const_raw_ptr_deref))]
 
 use core::cell::UnsafeCell;
 use core::ops::{Deref, DerefMut};
@@ -20,6 +21,13 @@ impl<T: ?Sized> SharedMut<T> {
     #[inline]
     pub const fn get(&self) -> *mut T {
         self.0
+    }
+}
+
+impl<T: Sized> From<*mut T> for SharedMut<T> {
+    #[inline]
+    fn from(ptr: *mut T) -> Self {
+        Self::new(ptr)
     }
 }
 
@@ -50,7 +58,7 @@ unsafe impl<T: ?Sized + Send> Send for RacyCell<T> { }
 
 #[cfg(feature = "const-default")]
 impl<T: const_default::ConstDefault> const_default::ConstDefault for RacyCell<T> {
-    const DEFAULT: Self = RacyCell { inner: UnsafeCell::new(T::DEFAULT) };
+    const DEFAULT: Self = RacyCell::new(T::DEFAULT);
 }
 
 impl<T: Sized> RacyCell<T> {
@@ -62,24 +70,141 @@ impl<T: Sized> RacyCell<T> {
     }
 }
 
+impl<T: Sized> From<T> for RacyCell<T> {
+    #[inline]
+    fn from(inner: T) -> Self {
+        Self::new(inner)
+    }
+}
+
 impl<T: ?Sized> RacyCell<T> {
     #[inline]
-    pub const fn inner(&self) -> &UnsafeCell<T> {
+    pub const fn racy_inner(&self) -> &UnsafeCell<T> {
         &self.inner
     }
 
     #[inline]
-    pub const fn get(&self) -> *mut T {
+    pub const fn racy_ptr(&self) -> *mut T {
         self.inner.get()
     }
 
     #[inline]
-    pub unsafe fn get_ref(&self) -> &T {
-        &*self.get()
+    #[cfg(feature = "unstable")]
+    pub const unsafe fn racy_ref(&self) -> &T {
+        &*self.racy_ptr()
     }
 
     #[inline]
-    pub unsafe fn get_mut(&self) -> &mut T {
-        &mut *self.get()
+    #[cfg(not(feature = "unstable"))]
+    pub unsafe fn racy_ref(&self) -> &T {
+        &*self.racy_ptr()
+    }
+
+    #[inline]
+    pub unsafe fn racy_mut(&self) -> &mut T {
+        &mut *self.racy_ptr()
+    }
+}
+
+impl<T: ?Sized + Sync> Deref for RacyCell<T> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            self.racy_ref()
+        }
+    }
+}
+
+#[derive(Debug)]
+#[repr(transparent)]
+pub struct RecklessCell<T: ?Sized> {
+    inner: RacyCell<T>,
+}
+
+unsafe impl<T: ?Sized> Sync for RecklessCell<T> { }
+unsafe impl<T: ?Sized> Send for RecklessCell<T> { }
+
+#[cfg(feature = "const-default")]
+impl<T: const_default::ConstDefault> const_default::ConstDefault for RecklessCell<T> {
+    const DEFAULT: Self = RecklessCell::new(T::DEFAULT);
+}
+
+impl<T: Sized> RecklessCell<T> {
+    #[inline]
+    pub const fn new(inner: T) -> Self {
+        // TODO make this an unsafe fn?
+        Self {
+            inner: RacyCell::new(inner),
+        }
+    }
+}
+
+impl<T: Sized> From<T> for RecklessCell<T> {
+    #[inline]
+    fn from(inner: T) -> Self {
+        Self::new(inner)
+    }
+}
+
+impl<T: ?Sized> RecklessCell<T> {
+    #[inline]
+    pub const fn reckless_inner(&self) -> &RacyCell<T> {
+        &self.inner
+    }
+
+    #[inline]
+    pub const fn reckless_cell(&self) -> &UnsafeCell<T> {
+        self.inner.racy_inner()
+    }
+
+    #[inline]
+    pub const fn reckless_get(&self) -> *mut T {
+        self.inner.racy_ptr()
+    }
+
+    #[inline]
+    #[cfg(feature = "unstable")]
+    pub const fn reckless_ref(&self) -> &T {
+        unsafe {
+            self.inner.racy_ref()
+        }
+    }
+
+    #[inline]
+    #[cfg(not(feature = "unstable"))]
+    pub fn reckless_ref(&self) -> &T {
+        unsafe {
+            self.inner.racy_ref()
+        }
+    }
+
+    #[inline]
+    pub unsafe fn reckless_mut(&self) -> &mut T {
+        self.inner.racy_mut()
+    }
+
+    #[inline]
+    pub fn reckless_get_mut(&mut self) -> &mut T {
+        unsafe {
+            self.reckless_mut()
+        }
+    }
+}
+
+impl<T: ?Sized> Deref for RecklessCell<T> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.reckless_ref()
+    }
+}
+
+impl<T: ?Sized> DerefMut for RecklessCell<T> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.reckless_get_mut()
     }
 }
